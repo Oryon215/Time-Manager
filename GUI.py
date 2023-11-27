1fba05beb0d0af967a692fbd947be16c5fe6cdd1
Oryon215 <ynon.slutzker@gmail.com>
2023-11-27 14:55:41 +0200

    Added automatic event removal alongside several improvements to scheduling algorithm

--- GUI.py
+++ GUI.py
@@ -5,35 +5,42 @@ from addons import *
 # GLOBALS
 root = Tk()
 main_frame = None
-types = {"Important and Urgent":r"Gama lol 26\11\2023 3", "Urgent":r"", "Important":r"", "Leisure":r""}
+type_events = {"Important and Urgent": r"Gama lol 26\11\2023 3", "Urgent": r"", "Important": r"", "Leisure": r""}
 free_sessions = "2 8\\2 11"
 settings = {"Frequency": 24, "break length": 30}
+
+
+def back_button(frame):
+    # a general pattern for creating the backward button
+    frame.forget()
+    start_main()
+# done
 
 
 # add events frame
 def start_type_windows():
     global main_frame
     global root
-    global types
+    global type_events
 
     f = Frame(root, bg="light green", height=600, width=520)
     lst = Listbox(f, selectmode="single", width=20, height=8)
-    for t in types:
+
+    for t in type_events:
         lst.insert(END, t)
     lst.place(x=60, y=10)
-    l = Label(f, width=17, height=5)
-    l.place(x=60, y=85)
+    label_for_events = Label(f, width=17, height=5)
+    label_for_events.place(x=60, y=85)
 
-    def lst_box(t):
+    def lst_box(interrrupt):
         i = lst.curselection()
-        l.config(text=types[lst.get(i)].replace("$", "\n"), anchor="center")
+        print(lst.get(i))
+        label_for_events.config(text=type_events[lst.get(i)].replace("$", "\n"), anchor="center")
+
     lst.bind("<<ListboxSelect>>", lst_box)
 
-    def back_button():
-        f.forget()
-        start_main()
-    b = Button(f, text="back", command= lambda: back_button())
-    b.place(x=10, y=290)
+    back = Button(f, text="back", command=lambda: back_button(f))
+    back.place(x=10, y=290)
 
     event_params = ["name", "description", "deadline", "time"]
     event_entries = []
@@ -42,12 +49,15 @@ def start_type_windows():
     y = 170
 
     for param in event_params:
-        l1 = Label(f, text=param, height=1, width=8)
-        l1.place(x=x, y=y)
-        e1 = Entry(f, validate="key", validatecommand=(root.register(validate_deadline), '%P'))
-        e1.place(x=x + 70, y=y)
+        param_label = Label(f, text=param, height=1, width=8)
+        param_label.place(x=x, y=y)
+        e1 = Entry(f)
         if param == "deadline":
-            e1.insert(0, "11\\11\\1111")
+            e1 = Entry(f, validate="key", validatecommand=(root.register(validate_deadline), '%P'))
+            # %P - the value that the text will have if allowed
+            # "KEY" - validate whenever a keystroke changes the widget's contents
+            e1.insert(0, "11\\11\\1111")  # Default value
+        e1.place(x=x + 70, y=y)
         event_entries.append(e1)
         y += 20
 
@@ -55,15 +65,17 @@ def start_type_windows():
         string = ""
         for entry in event_entries:
             string += entry.get() + " "
-        count_dollar = 0 if len(types[lst.get(ACTIVE)]) == 0 else 1
-        types[lst.get(ACTIVE)] += "$ "*count_dollar + string[:-1]
+        count_dollar = 0 if len(type_events[lst.get(ACTIVE)]) == 0 else 1  # Count how many separators are necessary
+        type_events[lst.get(ACTIVE)] += "$ " * count_dollar + string[:-1]
 
         lst_box(0)  # 0 is not an important value
-    b = Button(f, text="save", command=save_button)
-    b.place(x=126, y=260)
+
+    save = Button(f, text="save", command=save_button)
+    save.place(x=126, y=260)
 
     f.pack()
     main_frame.forget()
+# done
 
 
 # add session frame
@@ -74,53 +86,59 @@ def session_button():
 
     f = Frame(root, bg="orange", height=600, width=520)
     mylist = Listbox(f)
-    for session in free_sessions.split("\\"):
-        components = session.split(" ")
-        mylist.insert(END, f"{components[1]}:{int(components[0]) + int(components[1])}")
-    mylist.place(x = 80, y= 10)
-    e1 = Entry(f)
-    e1.insert(0, "00:00")
-    e1.place(x=80, y=150)
 
-    def back_button():
-        f.forget()
-        start_main()
-    b = Button(f, text="back", command=back_button)
-    b.place(x=10, y=290)
+    def print_lst():
+        global free_sessions
+        mylist.delete(0, END)
+        for session in free_sessions.split("\\"):
+            components = session.split(" ")
+            start_hour = components[1]
+            finish_hour = int(components[0]) + int(components[1])
+            mylist.insert(END, f"{start_hour}:{finish_hour}")
+    print_lst()
+
+    mylist.place(x=80, y=10)
+    new_session_entry = Entry(f, validate="key", validatecommand=(root.register(validate_session), '%P'))
+    new_session_entry.insert(0, "00:00")
+    new_session_entry.place(x=80, y=150)
+
+    back = Button(f, text="back", command=lambda: back_button(f))
+    back.place(x=10, y=290)
 
     def save_button():
         global free_sessions
-        text = e1.get()
-        if ":" in text:
-            parts = text.split(":")
-            length = int(parts[1]) - int(parts[0])
-            length = str(length)
-            start = str(int(parts[0]))
-            components = free_sessions.split("\\")
-            for i in range(len(components)):
-                length2, start2 = components[i].split(" ")
-                end2 = start2 + length2
-                if not overlaping_ranges((start2, end2), (start, start+length)):
+        text = new_session_entry.get()
+        hours = text.split(":")
+        start_hour = int(hours[0])
+        end_hour = int(hours[1])
+        length_session = int(end_hour) - int(start_hour)
 
-                    if int(start2) > int(start):
-                        components.insert(i, f"{length} {start}")
-                        free_sessions = "\\".join(components)
-                        mylist.delete(0, END)
-                        for session in free_sessions.split("\\"):
-                            components = session.split(" ")
-                            mylist.insert(END, f"{components[1]}:{int(components[0]) + int(components[1])}")
-                        return
-            components.append(f"{length} {start}")
-            free_sessions = "\\".join(components)
-            mylist.delete(0, END)
-            for session in free_sessions.split("\\"):
-                components = session.split(" ")
-                mylist.insert(END, f"{components[1]}:{int(components[0]) + int(components[1])}")
-    B = Button(f,bg="gray", text="add", command=save_button)
-    B.place(x=80, y=180)
+        sessions = free_sessions.split("\\")
+        for i in range(len(sessions)):
+            components = sessions[i].split(" ")
+            length_old = int(components[0])
+            start_hour_old = int(components[1])
+            end_hour_old = start_hour_old + length_old
+
+            if start_hour_old > start_hour:
+                if not overlaping_ranges((start_hour_old, end_hour_old), (start_hour, end_hour)):
+                    sessions.insert(i, f"{length_session} {start_hour}")
+                    free_sessions = "\\".join(sessions)
+                    print_lst()
+                    new_session_entry.delete(0, END)
+                    return
+
+        sessions.append(f"{length_session} {start_hour}")
+        free_sessions = "\\".join(sessions)
+        new_session_entry.delete(0, END)
+        print_lst()
+
+    save = Button(f, bg="gray", text="add", command=save_button)
+    save.place(x=80, y=180)
 
     f.pack()
     main_frame.forget()
+# make sure last range doesnt overlap with new one
 
 
 # start menu frame
@@ -128,22 +146,21 @@ def start_main():
     global main_frame
     global root
     main_frame = Frame(root, bg="light blue", height=600, width=520)
+    buttons = {"add events": start_type_windows, "add free sessions": session_button, "schedule": schedule_day,
+               "settings": setting_button}
+
+    # set initial starting position
     x = 10
     y = 10
-    B1 = Button(main_frame, text="add events", height=2, width=30, bg="light gray", command= start_type_windows)
-    B1.place(x=x, y=y)
-    y += 60
-    B2 = Button(main_frame, text="add free sessions", height=2, width=30, bg="light gray", command=session_button)
-    B2.place(x=x, y=y)
-    y += 60
 
-    B3 = Button(main_frame, text="schedule", height=2, width=30, bg="light gray", command=schedule_day)
-    B3.place(x=x, y=y)
-    y += 60
+    for button_text in buttons.keys():
+        button_function = buttons[button_text]
+        button = Button(main_frame, text=button_text, height=2, width=30, bg="light gray", command=button_function)
+        button.place(x=x, y=y)
+        y += 60
 
-    B4 = Button(main_frame, text="settings", height=2, width=30, bg="light gray", command=setting_button)
-    B4.place(x=x, y=y)
     main_frame.pack()
+# done
 
 
 # schedule option frame
@@ -151,45 +168,43 @@ def schedule_day():
     global settings
     global main_frame
     global root
-    global types
+    global type_events
     global free_sessions
     import subprocess
 
     listevents = ""
     importance = 3
-    for t in types.keys():
-        st = types[t]
+    for t in type_events.keys():
+        st = type_events[t]
         if st != "":
             st = st.replace("$", f"${importance}")
             listevents += f"${importance} " + st
             importance -= 1
-    text = subprocess.run(["TimeManagementSource.exe", "", listevents, free_sessions], capture_output = True, text=True)
+    text = subprocess.run(["TimeManagementSource.exe", "", listevents, free_sessions], capture_output=True, text=True)
 
     f = Frame(root, bg="light blue", height=1000, width=1000)
     print(text.stderr)
 
-    schedule_data = parse_schedule(text.stdout)
+    return_value = text.stdout.split("-") # split into actual schedule and left events
+    schedule_data = parse_schedule(return_value[0])
+    if len(return_value) > 1:
+        type_events = reset_event_lst(return_value[1], type_events)
 
     header = ["Time", "Event", "Type", "Deadline", "Duration"]
     for i, text in enumerate(header):
         Label(f, text=text, font=("Helvetica", 10, "bold")).grid(row=0, column=i, padx=5, pady=5)
     for row, data in enumerate(schedule_data, start=1):
-        tk.Label(f, text=data["hour"]).grid(row=row, column=0, padx=5, pady=5)
-        tk.Label(f, text=data["name"]).grid(row=row, column=1, padx=5, pady=5)
-        tk.Label(f, text=data["type"]).grid(row=row, column=2, padx=5, pady=5)
-        tk.Label(f, text=data["deadline"]).grid(row=row, column=3, padx=5, pady=5)
-        tk.Label(f, text=str(data["time"])).grid(row=row, column=4, padx=5, pady=5)
+        Label(f, text=data["hour"]).grid(row=row, column=0, padx=5, pady=5)
+        Label(f, text=data["name"]).grid(row=row, column=1, padx=5, pady=5)
+        Label(f, text=data["type"]).grid(row=row, column=2, padx=5, pady=5)
+        Label(f, text=data["deadline"]).grid(row=row, column=3, padx=5, pady=5)
+        Label(f, text=str(data["time"])).grid(row=row, column=4, padx=5, pady=5)
+
+    back = Button(f, text="back", command=lambda: back_button(f))
+    back.place(x=0, y=290)
     f.pack()
     main_frame.forget()
-
-
-    def back_button():
-        f.forget()
-        start_main()
-    b = Button(f, text="back", command=back_button)
-    b.place(x=0, y=290)
-    f.pack()
-    main_frame.forget()
+# try to shorten schedule printing code
 
 
 # settings option
@@ -197,13 +212,13 @@ def setting_button():
     global main_frame
     global settings
 
-    f = Frame(root, bg="gray", height =600, width=520)
+    f = Frame(root, bg="gray", height=600, width=520)
     y = 10
     entry_widgets = []
     for setting in settings.keys():
-        l = Label(f, text=f"{setting}:")
-        l.place(x=10, y=y)
-        e = Entry(f, width = 2)
+        setting_label = Label(f, text=f"{setting}:")
+        setting_label.place(x=10, y=y)
+        e = Entry(f, width=2)
         entry_widgets.append((setting, e))
         e.insert(0, f"{settings[setting]}")
         e.place(x=90, y=y)
@@ -212,24 +227,26 @@ def setting_button():
     def save_button():
         for entry in entry_widgets:
             settings[entry[0]] = entry[1].get()
-    B = Button(f,bg="orange", text="save", command=save_button)
-    B.place(x=10, y=y)
 
-    def back_button():
-        f.forget()
-        start_main()
-    b = Button(f, text="back", command=back_button)
-    b.place(x=10, y=290)
+    save = Button(f, bg="orange", text="save", command=save_button)
+    save .place(x=10, y=y)
+
+    back = Button(f, text="back", command=lambda: back_button(f))
+    back.place(x=10, y=290)
 
     f.pack()
     main_frame.forget()
+# done - but make sure to add implementation of settings to exe and schedule day
 
 
 def main():
     global free_sessions
+    global settings
+
     root.title("Time Manager")
     root.geometry("300x360")
     root.configure(bg="light blue")
+
     with open("./save_files/settings.txt", "r") as fd:
         lines = fd.readline()
         prev = ''
@@ -243,29 +260,17 @@ def main():
         lines = fd.readlines()
         prev = ''
         for line in lines:
-            if prev[:-1] in list(types.keys()):
-                types[prev[:-1]] = line[:-1]
+            if prev[:-1] in list(type_events.keys()):
+                type_events[prev[:-1]] = line[:-1]
             prev = line
 
-    # Place initial buttons
-    x = 20
-    y = 10
     start_main()
     tk.mainloop()
-    with open("./save_files/settings.txt", "w") as fd:
-        s = ''
-        for key in settings.keys():
-            s += f"{key}\n"
-            s += str(settings[key]) + "\n"
-        fd.write(s)
-
+    # save info to files
+    write_dictionary(settings, r"./save_files/settings.txt")
     with open("./save_files/sessions.txt", "w") as fd:
         fd.write(free_sessions)
+    write_dictionary(type_events, r"./save_files/events.txt")
 
-    with open("./save_files/events.txt", "w") as fd:
-        s = ''
-        for key in types.keys():
-            s += f"{key}\n"
-            s += types[key]
-            s += "\n"
-        fd.write(s)
+
+# try to add functions for file I/O
--- addons.py
+++ addons.py
@@ -1,5 +1,19 @@
+def type_to_int(t:str):
+    match t:
+        case "Imporant and Urgent":
+            return 3
+        case "Urgent":
+            return 2
+        case "Important":
+            return 1
+        case "Leisure":
+            return 0
+    return -1
 
-def overlaping_ranges(range1: tuple[int], range2: tuple[int]):
+
+
+def overlaping_ranges(range1: tuple[int, int], range2: tuple[int, int]):
+    # Check if a new session hour range overlaps a previous session hour range
     f_in = lambda a, b, c: a < c < b
     for number1 in range1:
         if f_in(range2[0], range2[1], number1):
@@ -9,7 +23,9 @@ def overlaping_ranges(range1: tuple[int], range2: tuple[int]):
             return True
     return False
 
+
 def validate_deadline(new_value):
+    # Make sure that the date given is valid
     import datetime
     # Check if the new value has a length less than or equal to 8
     if len(new_value) <= 10 and (len(new_value) < 6 or (new_value[2] == '\\' and new_value[5] == '\\')):
@@ -22,19 +38,91 @@ def validate_deadline(new_value):
     return False
 
 
+def validate_session(hour_range):
+    separator = ":"
+    # Make sure that the session given is valid
+    if len(hour_range) > 2:
+        if hour_range[2] != separator:
+            return False
+    if len(hour_range) > 4:
+        start_hour = hour_range[:2]
+        finish_hour = hour_range[3:]
+        if not (start_hour.isnumeric() or finish_hour.isnumeric()):
+            return False
+        start_hour = int(start_hour)
+        finish_hour = int(finish_hour)
+        if not (0 < start_hour < 23 or 0 < finish_hour < 23):
+            return False
+        if start_hour > finish_hour:
+            return False
+    return True
+
+
 def parse_schedule(schedule: str):
+    """
+
+    :param schedule: a string representing the schedule after it has been created by the exe
+    :return: a lst of dictionaries representing events
+    """
     events = schedule.split("\n")
     schedule_lst = []
     for event in events[:-2]:  # dont include /n
-        data = event.split(",")
-        print("data:",data)
-        hour, name = ":".join(data[0].split(":")[0:2]), data[0].split(":")[2]
-        type_event = data[1].split(":")[1]
-        deadline = data[2].split(":")[1]
-        description = data[3]
-        time = data[4]
-
-        event_data = {"hour": hour, "name": name, "type": type_event, "deadline": deadline, "description": description, "time": time}
+        event_data = parse_event(event)
 
         schedule_lst.append(event_data)
     return schedule_lst
+
+
+def parse_event(event: str) -> dict:
+    data = event.split(",")
+    hour, name = ":".join(data[0].split(":")[0:2]), data[0].split(":")[2]
+    type_event = data[1].split(":")[1]
+    deadline = data[2].split(":")[1]
+    description = data[3]
+    time = str(int(data[4]) / 60)
+
+    event_data = {"hour": hour, "name": name, "type": type_event, "deadline": deadline, "description": description,
+                  "time": time}
+    return event_data
+
+
+def write_dictionary(dic: dict, path: str):
+    # write dictionary to file in path
+    with open(path, "w") as fd:
+        s = ''
+        for key in dic.keys():
+            s += f"{key}\n"
+            s += str(dic[key])
+            s += "\n"
+        fd.write(s)
+
+
+def reset_event_lst(events: str, current_events: dict):
+    print(events)
+    print(current_events)
+    new_events = dict()
+    for key in current_events.keys():
+        new_events[key] = ''
+    lines = events.split("\n")
+    lines = filter(lambda x: x != '', lines)
+    prev = ''
+    for line in lines:
+        if line[:-1] in current_events.keys(): # dont include the ":" in the original text
+            prev = line[:-1]
+        elif prev in current_events.keys():
+            line_fixed = "00:00:" + line
+            event_dict = parse_event(line_fixed)
+            simple_str = f"{event_dict["name"]} {event_dict["description"]} {event_dict["deadline"]} {event_dict["time"]}"
+            new_events[prev] +=  simple_str
+    print(new_events)
+    return new_events
+
+
+
+
+
+
+
+
+
+
